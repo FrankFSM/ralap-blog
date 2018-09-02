21a96101f2bb1ac579b6100a2594b7d3863efe26
haojixiang <hao_jixiang@163.com>
2018-09-02 11:23:40 +0800

    电纸书后台编辑

--- blog-core/src/main/java/com/ralap/blog/persistence/beans/BizEbook.java
+++ blog-core/src/main/java/com/ralap/blog/persistence/beans/BizEbook.java
@@ -1,9 +1,12 @@
 package com.ralap.blog.persistence.beans;
 
-import java.util.Date;
+import com.ralap.blog.framework.object.AbstractDO;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
-public class BizEbook {
-    private Long id;
+@Data
+@EqualsAndHashCode(callSuper = false)
+public class BizEbook extends AbstractDO {
 
     private String title;
 
@@ -16,80 +19,4 @@ public class BizEbook {
     private Boolean status;
 
     private String description;
-
-    private Date createTime;
-
-    private Date updateTime;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title == null ? null : title.trim();
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author == null ? null : author.trim();
-    }
-
-    public String getCoverImage() {
-        return coverImage;
-    }
-
-    public void setCoverImage(String coverImage) {
-        this.coverImage = coverImage == null ? null : coverImage.trim();
-    }
-
-    public String getPath() {
-        return path;
-    }
-
-    public void setPath(String path) {
-        this.path = path == null ? null : path.trim();
-    }
-
-    public Boolean getStatus() {
-        return status;
-    }
-
-    public void setStatus(Boolean status) {
-        this.status = status;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description == null ? null : description.trim();
-    }
-
-    public Date getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(Date createTime) {
-        this.createTime = createTime;
-    }
-
-    public Date getUpdateTime() {
-        return updateTime;
-    }
-
-    public void setUpdateTime(Date updateTime) {
-        this.updateTime = updateTime;
-    }
 }
