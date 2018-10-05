ee9340082422076e01e7350b1e030b2af9066ae3
haojixiang <hao_jixiang@163.com>
2018-10-05 20:38:06 +0800

    格式化

--- blog-core/src/main/java/com/ralap/blog/util/AesUtil.java
+++ blog-core/src/main/java/com/ralap/blog/util/AesUtil.java
@@ -29,26 +29,23 @@ import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 
 /**
- *
  * @author yadong.zhang (yadong.zhang0415(a)gmail.com)
- * @website https://www.zhyd.me
  * @version 1.0
+ * @website https://www.zhyd.me
  * @date 2018/4/16 16:26
  * @since 1.0
  */
 public class AesUtil {
+
     private static final String KEY_ALGORITHM = "AES";
     private static final String DEFAULT_CIPHER_ALGORITHM = "AES/ECB/PKCS5Padding";
 
     /**
      * AES加密
      *
-     * @param passwd
-     *         加密的密钥
-     * @param content
-     *         需要加密的字符串
+     * @param passwd 加密的密钥
+     * @param content 需要加密的字符串
      * @return 返回Base64转码后的加密数据
-     * @throws Exception
      */
     public static String encrypt(String passwd, String content) throws Exception {
         // 创建密码器
@@ -69,12 +66,9 @@ public class AesUtil {
     /**
      * AES解密
      *
-     * @param passwd
-     *         加密的密钥
-     * @param encrypted
-     *         已加密的密文
+     * @param passwd 加密的密钥
+     * @param encrypted 已加密的密文
      * @return 返回解密后的数据
-     * @throws Exception
      */
     public static String decrypt(String passwd, String encrypted) throws Exception {
         //实例化
@@ -91,10 +85,9 @@ public class AesUtil {
 
     /**
      * 生成加密秘钥
-     *
-     * @return
      */
-    private static SecretKeySpec getSecretKey(final String password) throws NoSuchAlgorithmException {
+    private static SecretKeySpec getSecretKey(final String password)
+            throws NoSuchAlgorithmException {
         //返回生成指定算法密钥生成器的 KeyGenerator 对象
         KeyGenerator kg = KeyGenerator.getInstance(KEY_ALGORITHM);
         // javax.crypto.BadPaddingException: Given final block not properly padded解决方案
